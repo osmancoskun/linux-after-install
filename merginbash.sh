c0b9d3c39788e6adb53438941f06627ea8316a49
osmancoskun <osman.coskun95@gmail.com>
2021-04-11 13:20:55 +0300

    keyfime gore

---
+++ merginbash.sh
@@ -0,0 +1,8 @@
+## MERGIN AFTER INSTALL
+
+# ONCELIKLE BIR GUNCELLESTIRME
+yes | sudo pacman -Syyu
+
+# SONRA GEREKLI SUREKLI KULLANDIKLARIMIZI KURUYORUZ
+pamac install --no-confirm google-chrome base base-devel code github-cli git npm nodejs steam discord peek
+
