589766a1e0707699a3f891cf99e8677f7b84b815
osmancoskun <osman.coskun95@gmail.com>
2021-04-12 20:59:49 +0300

    obs added

--- merginbash.sh
+++ merginbash.sh
@@ -4,7 +4,7 @@
 yes | sudo pacman -Syyu
 
 # SONRA GEREKLI SUREKLI KULLANDIKLARIMIZI KURUYORUZ
-pamac install --no-confirm google-chrome base base-devel code github-cli git npm nodejs steam discord peek telegram-desktop
+pamac install --no-confirm google-chrome base base-devel code github-cli git npm nodejs steam discord peek telegram-desktop obs-studio
 
 # VUEJS DEV SEYSI
 sudo npm install -g @vue/cli
